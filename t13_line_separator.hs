7c234e5c7396d0278532481263808be888e2d3d2
Sebastian Kramer <sebastian.kramer@wp.pl>
2015-01-27 22:56:05 +0100

    minor

--- t13_line_separator.hs
+++ t13_line_separator.hs
@@ -1,11 +1,11 @@
+lineSplit cs = let (pre:rest:[]) = break isSentinel cs 
 lineSplit :: String -> [String]
 lineSplit cs = let (pre,rest) = break isSentinel cs
-{- lineSplit cs = let (pre:rest:[]) = break isSentinel cs -}
                 in pre: case rest of
-                    (rest:"\n")     -> lineSplit rest
-                    (rest:"\r")     -> lineSplit rest
-                    (rest:"\r\n")   -> lineSplit rest
-                    _               -> []
+                             ('\r':'\n':rest)   -> lineSplit rest
+                             ('\n':rest)     -> lineSplit rest
+                             ('\r':rest)     -> lineSplit rest
+                             _               -> []
 
 isSentinel x = x == '\n' || x == '\r'
 
